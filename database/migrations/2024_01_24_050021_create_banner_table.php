e5ccb15cd7d41cf113df93ae0d6106ec15646a48
alden <connect@alenev.name>
2024-01-24 08:06:20 +0200

    created DB migrations

---
+++ database/migrations/2024_01_24_050021_create_banner_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('status_id');
+            $table->string('name');
+            $table->text('text');
+            $table->string('url');
+            $table->timestamps();
+    
+            $table->foreign('status_id')->references('id')->on('statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+};
